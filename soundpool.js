8724c9196c461bec5f85b5f69e97067f4ba7e63c
Peterson Trethewey <peterson.trethewey@gmail.com>
2016-01-12 14:32:35 -0800

    Moved soundpool to its own file

---
+++ soundpool.js
@@ -0,0 +1,62 @@
+
+var SoundPool = SoundPool || function()
+{
+    this.buffers = [null, null, null, null, null, null, null, null];
+    this.index = 0;
+};
+
+SoundPool.prototype.add = function(buffer, trackIndex, finishedCallback)
+{
+    this.buffers[trackIndex] = {
+        start: this.index,
+        buffer: buffer,
+        numSamples: buffer.length,
+        finished: finishedCallback
+    };
+};
+
+SoundPool.prototype.addAtTime = function(buffer, trackIndex, finishedCallback)
+{
+    this.buffers[trackIndex] = {
+        start: ,
+        buffer: buffer,
+        numSamples: buffer.length,
+        finished: finishedCallback
+    };
+};
+
+SoundPool.prototype.sample = function(array0, array1, arrayIndex)
+{
+    array0[arrayIndex] = 0;
+    array1[arrayIndex] = 0;
+
+    for(var i = 0; i < this.buffers.length; i++)
+    {
+        var buffer = this.buffers[i];
+        if( buffer )
+        {
+            var start = buffer.start;
+            var k = this.index-start;
+
+            if( k < 0 )
+            {
+                continue;
+            }
+
+            if( k > buffer.numSamples )
+            {
+                buffer.finished();
+                this.buffers[i] = null;
+                continue;
+            }
+
+            var inarray = buffer.buffer.getChannelData(0);
+
+            array0[arrayIndex] += inarray[k];
+            array1[arrayIndex] += inarray[k];
+        }
+    }
+
+    this.index++;
+};
+
